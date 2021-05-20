a9022153dc75726b729c7f3f106bb6afb50492e9
Sara Carlsson <sara.carlsson@yh.nackademin.se>
2021-05-20 13:51:58 +0200

    Add test for "findEmployeeByFirstNameAndLastName" in EmployeeReporitoryTest

---
+++ src/test/java/com/example/registeremployee/repositories/EmployeeRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.example.registeremployee.repositories;
+
+import com.example.registeremployee.models.Employee;
+import com.example.registeremployee.models.EmploymentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    void findEmployeeByFirstNameAndLastName() {
+        Employee expected = new Employee("Sara", "Carlsson", "Female",
+                "830208XXXX", 35000);
+        expected.setEmploymentType(new EmploymentType("Undersköterska"));
+
+        employeeRepository.save(expected);
+
+        List<Employee> actual = employeeRepository.findEmployeeByFirstNameAndLastName("Sara", "Carlsson");
+
+        assertEquals(actual.get(0), expected);
+    }
+}
