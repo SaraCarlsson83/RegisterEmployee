c9bff1d92b244672e9db49e683fa7e5ad1fb73df
I-Zor <ivona.zoricic@gmail.com>
2021-05-20 14:47:48 +0200

    Add test for findAll method in EmployeeService

---
+++ src/test/java/com/example/registeremployee/services/EmployeeServiceTest.java
@@ -0,0 +1,61 @@
+package com.example.registeremployee.services;
+
+import com.example.registeremployee.models.Employee;
+import com.example.registeremployee.models.EmploymentType;
+import com.example.registeremployee.repositories.EmployeeRepository;
+import com.example.registeremployee.repositories.EmploymentTypeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceTest {
+
+    EmployeeService service;
+
+    @Mock
+    EmployeeRepository mockRepository;
+
+    @Mock
+    EmploymentTypeRepository mockTypeRepository;
+
+    @BeforeEach
+    public void init() {
+        service = new EmployeeService(mockRepository, mockTypeRepository);
+    }
+
+    @Test
+    void findAll() {
+        Employee mockEmployee = new Employee();
+        mockEmployee.setFirstName("Ivona");
+        mockEmployee.setLastName("Zoricic");
+        mockEmployee.setGender("kvinna");
+        mockEmployee.setSocialSecurityNr("8307084445");
+        mockEmployee.setSalary(35000);
+        mockEmployee.setEmploymentType(new EmploymentType("Utvecklare"));
+
+        when(mockRepository.findAll())
+                .thenReturn(Arrays.asList(mockEmployee));
+
+        List<Employee> actual = service.findAll();
+
+        assertEquals(mockEmployee.getFirstName(), actual.get(0).getFirstName());
+        assertEquals(mockEmployee.getLastName(), actual.get(0).getLastName());
+        assertEquals(mockEmployee.getGender(), actual.get(0).getGender());
+        assertEquals(mockEmployee.getSocialSecurityNr(), actual.get(0).getSocialSecurityNr());
+        assertEquals(mockEmployee.getSalary(), actual.get(0).getSalary());
+        assertEquals(mockEmployee.getEmploymentType(), actual.get(0).getEmploymentType());
+
+        verify(mockRepository).findAll();
+
+    }
+}
