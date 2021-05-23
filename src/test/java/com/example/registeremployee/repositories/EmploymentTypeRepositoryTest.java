3777da304f4c8b4b9690d5f563f11c9df1d20442
abelshif <abelshif@gmail.com>
2021-05-23 18:51:37 +0200

    Added test for 'findByName'in  EmploymentTypeRepositoryTest

---
+++ src/test/java/com/example/registeremployee/repositories/EmploymentTypeRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.example.registeremployee.repositories;
+
+import com.example.registeremployee.models.EmploymentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataMongoTest
+class EmploymentTypeRepositoryTest {
+
+    @Autowired
+    EmploymentTypeRepository repository;
+
+    @Test
+    void findByName() {
+        String name = "Sjuksköterska";
+        EmploymentType expected  = new EmploymentType("Sjuksköterska");
+        repository.save(expected);
+
+        Optional<EmploymentType> actual = repository.findByName("Sjuksköterska");
+
+        assertEquals(expected,actual.orElseThrow());
+
+
+    }
+}
+
