9dc14239522e6908af9b05fac17245aa8f6cb3a4
sileshi haile <sileshih@gmail.com>
2021-05-24 15:06:02 +0100

    added test for addType method in

--- src/test/java/com/example/registeremployee/services/EmploymentTypeServiceTest.java
+++ src/test/java/com/example/registeremployee/services/EmploymentTypeServiceTest.java
@@ -25,13 +25,13 @@ class EmploymentTypeServiceTest {
     //EmploymentTypeService service;
 
     @Test
-    void findAll() {
-        EmploymentType expected = new EmploymentType("läkare");
-        //expected.setName("Undersköterska");
-        typeRepository.save(expected);
-        List<EmploymentType> actual= typeRepository.findAll();
-        assertEquals(actual.get(0),expected);
 
+        void addType() {
+            EmploymentType type = new EmploymentType("type1");
+            typeRepository.save(type).toString();
+            EmploymentType actual= typeRepository.findAll().get(0);
+
+            assertEquals(actual,type);
+
+        }
     }
-
-}
