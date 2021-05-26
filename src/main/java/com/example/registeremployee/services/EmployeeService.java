f451075e12f0cd57d967a5ea3852952a3be9a961
Sara Carlsson <sara.carlsson@yh.nackademin.se>
2021-05-26 18:13:06 +0200

    - changed image name in docker-compose-prod

--- src/main/java/com/example/registeremployee/services/EmployeeService.java
+++ src/main/java/com/example/registeremployee/services/EmployeeService.java
@@ -6,9 +6,7 @@ import com.example.registeremployee.repositories.EmployeeRepository;
 import com.example.registeremployee.repositories.EmploymentTypeRepository;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
--- src/test/java/com/example/registeremployee/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/example/registeremployee/repositories/EmployeeRepositoryTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
