b0ecec9cdc84ae4b02f39a9a967e441e628b1902
sileshi haile <sileshih@gmail.com>
2021-05-26 10:44:30 +0100

    added test method

--- src/test/java/com/example/registeremployee/services/EmploymentTypeServiceTest.java
+++ src/test/java/com/example/registeremployee/services/EmploymentTypeServiceTest.java
@@ -24,17 +24,38 @@ class EmploymentTypeServiceTest {
     EmploymentTypeRepository typeRepository;
     //EmploymentTypeService service;
 
-    @Test
+    /*@Test
     void deleteType() {
         EmploymentType expected = new EmploymentType("Undersköterska");
         //expected.setName("Undersköterska");
         typeRepository.save(expected);
 
-        EmploymentType actual =typeRepository.deleteByName("Undersköterska");
-        assertEquals(actual,expected);
+        //EmploymentType actual =typeRepository.deleteByName("Undersköterska");
+        assertEquals(actual,expected);*//*
 
         //Optional<EmploymentType> actual = typeRepository.deleteByName("");
 
-        // assertTrue(actual.isEmpty());
+        assertTrue(actual.isEmpty());
+    }*/
+    @Test
+    void findAll() {
+        EmploymentType expected = new EmploymentType("läkare");
+        //expected.setName("Undersköterska");
+        typeRepository.save(expected);
+        List<EmploymentType> actual= typeRepository.findAll();
+        assertEquals(actual.get(0),expected);
+
     }
+    @Test
+    void addType() {
+        EmploymentType type = new EmploymentType("type1");
+        typeRepository.save(type).toString();
+        EmploymentType actual= typeRepository.findAll().get(0);
+
+        assertEquals(actual,type);
+
     }
+}
+
+
+
