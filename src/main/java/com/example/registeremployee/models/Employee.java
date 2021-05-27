9e85a55dcbe9eda894566af4ec2a7891fcf8da36
sileshi haile <sileshih@gmail.com>
2021-05-27 12:33:20 +0100

    updated test method

--- src/main/java/com/example/registeremployee/models/Employee.java
+++ src/main/java/com/example/registeremployee/models/Employee.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
-import javax.persistence.*;
+
 
 @Data
 @AllArgsConstructor
@@ -14,21 +14,27 @@ import javax.persistence.*;
 @Accessors(chain = true)
 public class Employee {
 
-    @Id
-    @GeneratedValue
+
     private String id;
 
     private String firstName;
     private String lastName;
     private String gender;
 
-    @Column(unique = true)
+
     private String socialSecurityNr;
 
     private double salary;
 
-    @ManyToOne
-    @JoinColumn(referencedColumnName = "id")
+
     private EmploymentType employmentType;
     //private String department;
+    public Employee(String firstName, String lastName, String gender, String socialSecurityNr, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.socialSecurityNr = socialSecurityNr;
+        this.salary = salary;
+
+    }
 }
--- src/test/java/com/example/registeremployee/services/EmploymentTypeServiceTest.java
+++ src/test/java/com/example/registeremployee/services/EmploymentTypeServiceTest.java
@@ -2,9 +2,13 @@ package com.example.registeremployee.services;
 
 import com.example.registeremployee.models.EmploymentType;
 import com.example.registeremployee.repositories.EmploymentTypeRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 
 import java.util.List;
 import java.util.Optional;
@@ -36,23 +40,37 @@ class EmploymentTypeServiceTest {
         //Optional<EmploymentType> actual = typeRepository.deleteByName("");
 
         assertTrue(actual.isEmpty());
+
     }*/
+
+    @AfterEach
+     public  void init(){
+       typeRepository.deleteByName("läkare");
+    }
     @Test
     void findAll() {
-        EmploymentType expected = new EmploymentType("läkare");
-        //expected.setName("Undersköterska");
-        typeRepository.save(expected);
+
+       EmploymentType employmentType = new EmploymentType("läkare");
+        typeRepository.save(employmentType);
+
         List<EmploymentType> actual= typeRepository.findAll();
-        assertEquals(actual.get(0),expected);
+        System.out.println(employmentType.getId());
+
+        System.out.println(actual.get(0).getId() + "-" + actual.get(0).getName());
+
+
+        assertEquals(actual.get(0),employmentType);
 
     }
     @Test
     void addType() {
-        EmploymentType type = new EmploymentType("type1");
-        typeRepository.save(type).toString();
+
+        EmploymentType employmentType = new EmploymentType("läkare");
+        typeRepository.save(employmentType);
+
         EmploymentType actual= typeRepository.findAll().get(0);
 
-        assertEquals(actual,type);
+        assertEquals(actual,employmentType);
 
     }
 }
